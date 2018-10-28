7c5adb600079a17b7a049c7b86b12c817e950c21
maximoMichael <maximomichaelfx@gmail.com>
2018-10-28 01:52:47 -0300

    Inserido padrinho; Atualização arquivo Json

---
+++ Assets/Scripts/Events/Eventos para UnityEvents/UnityEvent_Padrinho.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+//Maximo Michael
+//28 de outubro de 2018 01:34hs
+
+public class UnityEvent_Padrinho : MonoBehaviour {
+    //Array capturar Caminho;
+    public Transform [] C4_P = new Transform[0];
+    //Contar o caminho percorrido;
+    public int countCaminho = 0;
+    //Impontar valores abaixo;
+    NavMeshAgent navMeshAgente;
+    Transform transformPersonagem;
+    Collider colliderPersonagem;
+
+    void Start() {
+        navMeshAgente = GetComponent<NavMeshAgent>();
+        transformPersonagem = GetComponent<Transform>();
+        colliderPersonagem = GetComponent<Collider>();
+    }
+
+    public void movePadrinho()
+   {
+        if (C4_P.Length-1 >= countCaminho)
+        {
+            //enabledCollider();
+            countCaminhoDef();
+            //Movimentar para o ponto expecificado pelo countCaminho
+            navMeshAgente.destination = C4_P[countCaminho].position;            
+        }
+        
+   }
+   public void countCaminhoDef()
+    {
+        //Verificar se o npc chegou no ponto expecificado;
+        if (C4_P[countCaminho].position.x == transformPersonagem.position.x && C4_P[countCaminho].position.z == transformPersonagem.position.z)
+        {
+            //Somar mais um;
+            countCaminho++;
+            //enabledCollider();
+        }
+    }
+
+    void enabledCollider()
+    {
+        //desabilitar Collider;
+        colliderPersonagem.enabled = !colliderPersonagem;
+    }
+}
