3147c5d5e8a54984d78604207ccc6d2f06d14d98
maximoMichael <maximomichaelfx@gmail.com>
2018-10-28 22:38:51 -0300

    Atualização Padrinho

---
+++ Assets/Scripts/Events/UnityEvents/UnityEvent_Aminator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+public class UnityEvent_Aminator : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator npcAnimator;
+    public string[] varFloat;
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        npcAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (varFloat.Length != 0)
+        {
+            animatorDef();
+        }
+    }
+    void animatorDef()
+    {
+        for (int countArray = 0; countArray <= varFloat.Length - 1; countArray++)
+        {
+            npcAnimator.SetFloat(varFloat[countArray], agent.velocity.magnitude);
+        }
+    }
+}
