a3c1fb3ccbce7412d107a3630f1552d6322b6177
Joao Trindade <jgltrindade@gmail.com>
2018-10-30 20:02:34 -0200

    scripts relacionados ao canvas comentados e um pouco alterados

--- Assets/Scripts/Manager/Manager.cs
+++ Assets/Scripts/Manager/Manager.cs
@@ -1,22 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
 
-public class Manager : MonoBehaviour {
-
+/*
+ * Praticamente tudo que esse script faz eh permitir que o jogador nao se mova
+ * quando clicado em algum lugar da tela atravez do metodo StopPlayerControlls(bool).
+ * 
+ * [@30/10/18] by [Joao G. L. Trindade]
+ * [1] Nomes de metodos corrigidos.
+ * [2] Codigo comentado para explicacao e remocao de trechos sem funcao aparente.
+ * [3] Criado metodo InstanceGuarantee() para limpeza de codigo.
+ */
+public class Manager : MonoBehaviour
+{
     public static Manager instance;
-
     public GameObject player;
 
-    private NavMeshSurface surface;
-    private Transform currentTrans;
-
+    //private NavMeshSurface surface;
+    //private Transform currentTrans;
 
     private void Start()
     {
-        #region SingleTon
+        InstanceGuarantee();
+
+        // JA ESTAVA COMENTADO:
+        //surface = GameObject.Find("Navmesh").GetComponent<NavMeshSurface>();
+    }
+
+    /* O QUE FAZ
+     * Garantindo a existencia de um unico Manager pelo jogo todo
+     * Provavelmente sera removido
+     */
+    private void InstanceGuarantee()
+    {
         if (instance == null)
         {
             instance = this;
@@ -26,34 +45,48 @@ public class Manager : MonoBehaviour {
         {
             Destroy(gameObject);
             return;
-        } 
-        #endregion
-
-        //surface = GameObject.Find("Navmesh").GetComponent<NavMeshSurface>();
-
+        }
     }
 
-    public void refreshNavMesh()
+    /* O QUE FAZ
+     * Metodo que impede o jogador de se mover caso clique em algum lugar.
+     */
+    public void StopPlayerControlls(bool stop)
     {
-        StartCoroutine(buildNavMesh());
+        player.GetComponent<controlPlay>().enabled = !stop;
+        Debug.Log(!stop);
     }
 
-    public void startDialog(Transform npcTrans, List<Dialog> dialogs)
+    /*
+     * A PARTIR DAQUI, NAO NOTEI NENHUMA DIFERENCA NO FUNCIONAMENTO DO JOGO SE
+     * COMPARA-LO COM QUANDO ESSES CODIGOS ESTAO ATIVOS. POR PRECAUCAO E PELO CODIGO JA ESTAR
+     * ESTREMAMENTE ENTRELACADO, DECIDI MANTE-LOS.
+     * POR FAVOR, ME AVISEM SE DESCOBRIREM UMA FUNCIONALIDADE OCULTA.
+     */
+
+    public void RefreshNavMesh()
     {
+        //StartCoroutine(BuildNavMesh());
+    }
+
+    public void StartDialog(Transform npcTrans, List<Dialog> dialogs)
+    {
+        /*
         currentTrans = npcTrans;
-        StartCoroutine(dialogCoroutine(dialogs));
+        StartCoroutine(DialogCoroutine(dialogs));
+        */
     }
 
-    IEnumerator buildNavMesh()
+    IEnumerator BuildNavMesh()
     {
         yield return new WaitForSeconds(0.1f);
-        //surface.BuildNavMesh();
+        //surface.BuildNavMesh();        
     }
-    IEnumerator dialogCoroutine(List<Dialog> dialogs)
+
+    IEnumerator DialogCoroutine(List<Dialog> dialogs)
     {
-
+        /*
         Transform anim = currentTrans.GetChild(0).GetChild(0);
-
 
         for (int i = 0; i < dialogs.Count; i++)
         {
@@ -64,19 +97,10 @@ public class Manager : MonoBehaviour {
             Debug.Log(dialogs[i].duration);
 
             yield return new WaitForSeconds(dialogs[i].duration);
-         
-
         }
 
         anim.GetComponent<Animator>().Play("FadeOut");
-
+        */
+        yield return null;
     }
-
-    public void StopPlayerControlls(bool stop)
-    {
-
-        player.GetComponent<controlPlay>().enabled = !stop;
-        Debug.Log(!stop);
-    }
-
 }
--- Assets/Scripts/Manager/OptionsPanel.cs
+++ Assets/Scripts/Manager/OptionsPanel.cs
@@ -1,22 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
-
+/*
+ * [@30/10/18] by [Joao G. L. Trindade]
+ * [1] Inicializacao da variavel booleana 'isShow' movida da declaracao para metodo Start()
+ * [2] Codigo comentado para melhor entendimento de suas funcoes.
+ */
 [RequireComponent(typeof(Animator))]
-public class OptionsPanel : MonoBehaviour {
-
+public class OptionsPanel : MonoBehaviour
+{
     public static OptionsPanel instance;
+    public bool isShow;
 
     private Animator anim;
-    [HideInInspector]
-	public bool isShow = false;
-    
+
     private void Start()
     {
-        if(instance == null)
+        isShow = false;
+        InstanceGuarantee();
+        anim = GetComponent<Animator>();
+    }
+
+    /* O QUE FAZ
+     * Garantindo a existencia de um unico OptionsPanel
+     * Provavelmente sera removido
+     */
+    private void InstanceGuarantee()
+    {
+        if (instance == null)
         {
             instance = this;
         }
@@ -24,35 +38,40 @@ public class OptionsPanel : MonoBehaviour {
         {
             return;
         }
-
-        anim = GetComponent<Animator>();
-
     }
 
-    
-    public Transform getTransform()
+    /* QUESTIONAMENTO
+     * Esse Getter pode ser removido futuramente?
+     * Um '.transform' nao faz a mesma coisa?
+     */
+    public Transform GetTransform()
     {
         return transform;
     }
 
-    public void clearOptions()
+    /* O QUE FAZ
+     * Destroi todas as childrens
+     */
+    public void ClearOptions()
     {
         for (int i = 0; i < transform.childCount; i++)
         {
             Destroy(transform.GetChild(i).gameObject);
         }
     }
-        
 
-    public void showOptions()
+    /* O QUE FAZ
+     * Controladores da pequena animacao de FadeIn e FadeOut dos botoes, para deixar sua aparicao mais sutil.
+     */
+    public void ShowOptions()
     {
         anim.Play("FadeIn");
-		isShow = true;
+        isShow = true;
     }
 
-    public void hideOptions()
+    public void HideOptions()
     {
         anim.Play("FadeOut");
-		isShow = false;
+        isShow = false;
     }
 }
