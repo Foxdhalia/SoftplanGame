7a90189bca88472d8726c6c1139114843a6c7299
maximoMichael <maximomichaelfx@gmail.com>
2018-11-04 22:02:04 -0200

    bug fix

---
+++ Assets/Scripts/UI/email.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class email : MonoBehaviour {
+    GameObject avisoEmail;
+    GameObject emailSoftPlan;
+    // Use this for initialization
+    void Start () {
+        avisoEmail = GetComponent<GameObject>();
+        emailSoftPlan = FindObjectOfType<GameObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void abrirEmail()
+    {
+        emailSoftPlan.gameObject.CompareTag("image_2");
+        Destroy(avisoEmail);
+    }
+}
