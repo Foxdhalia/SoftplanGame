14700d2306f0f9816accf214448f0fc912ef7a26
maximo <maximomichaelfx@gmail.com>
2019-01-17 20:55:53 -0200

    new scene

---
+++ Assets/Scripts/mensEmail.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mensEmail : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine("chamarMensagem");
+    }
+
+    IEnumerator chamarMensagem()
+    {
+        yield return new WaitForSeconds(4);
+        
+
+    }
+}
